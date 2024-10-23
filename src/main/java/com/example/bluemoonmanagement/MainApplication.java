9440491584a02e29438e0b683dde9de88efc4fd6
HuyEuler~ <duchuydtnn@gmail.com>
2024-10-23 19:14:31 +0700

    Update config GUI

--- src/main/java/com/example/bluemoonmanagement/MainApplication.java
+++ src/main/java/com/example/bluemoonmanagement/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/GUILogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 960, 760);
         stage.setTitle("Apartment Management System");
         stage.setScene(scene);
