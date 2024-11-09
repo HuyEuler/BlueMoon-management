27e4cda06282eb36fe6e38a873d12d6cbd694aa3
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-09 18:07:04 +0700

    no change

--- src/main/java/com/example/bluemoonmanagement/controllers/Login_Controller.java
+++ src/main/java/com/example/bluemoonmanagement/controllers/Login_Controller.java
@@ -28,6 +28,7 @@ public class Login_Controller {
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 }
