eda997e05c5417b209b3d1a5289bd6dea0059f35
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-09 18:09:47 +0700

    Specifies javafx file access to file Home_Controller.java in folder com.example.bluemoonmanagement.controllers.Home_management

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module com.example.bluemoonmanagement {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.xml;
+    requires java.sql;
 
     opens com.example.bluemoonmanagement to javafx.fxml;
     exports com.example.bluemoonmanagement;
 
     opens com.example.bluemoonmanagement.controllers to javafx.fxml;
     exports com.example.bluemoonmanagement.controllers;
-    exports com.example.bluemoonmanagement.controllers.Home_management;
+
     opens com.example.bluemoonmanagement.controllers.Home_management to javafx.fxml;
+    exports com.example.bluemoonmanagement.controllers.Home_management;
 }
