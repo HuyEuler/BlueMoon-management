414d749810a07c1842608703a0a84e41f5009697
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-10 13:56:26 +0700

    test Database

--- src/main/java/com/example/bluemoonmanagement/api/TestDBConnection.java
+++ src/main/java/com/example/bluemoonmanagement/api/TestDBConnection.java
@@ -25,31 +25,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+//public class TestDBConnection {
+//    public static void main(String[] args) {
+//        var url = "jdbc:mysql://localhost:3306/mydatabase";
+//        var user = "root";
+//        var password = "";
+//        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+//            System.out.println("Database: " + conn.getCatalog());
+//
+//            // Tạo câu lệnh SQL để truy vấn tất cả từ bảng tbl_student
+//            String query = "SELECT * FROM tbl_student";
+//
+//            // Tạo đối tượng Statement để thực hiện câu lệnh SQL
+//            try (Statement stmt = conn.createStatement()) {
+//                // Thực hiện câu lệnh SQL và lấy kết quả
+//                ResultSet rs = stmt.executeQuery(query);
+//
+//                // Duyệt qua kết quả và in ra các cột của mỗi bản ghi
+//                while (rs.next()) {
+//                    int id = rs.getInt("id");
+//                    String name = rs.getString("name");
+//                    String mail = rs.getString("email");  // Lấy giá trị của cột mail
+//                    float avgScore = rs.getFloat("avg_grade");  // Lấy giá trị của cột avg_score
+//                    System.out.println("ID: " + id + ", Name: " + name + ", Mail: " + mail + ", Avg Score: " + avgScore);
+//                }
+//            }
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//    }
+//}
+
 public class TestDBConnection {
     public static void main(String[] args) {
-        var url = "jdbc:mysql://localhost:3306/mydatabase";
+        var url = "jdbc:mysql://localhost:3306/mysql";
         var user = "root";
         var password = "";
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Database: " + conn.getCatalog());
-
-            // Tạo câu lệnh SQL để truy vấn tất cả từ bảng tbl_student
-            String query = "SELECT * FROM tbl_student";
-
-            // Tạo đối tượng Statement để thực hiện câu lệnh SQL
-            try (Statement stmt = conn.createStatement()) {
-                // Thực hiện câu lệnh SQL và lấy kết quả
-                ResultSet rs = stmt.executeQuery(query);
-
-                // Duyệt qua kết quả và in ra các cột của mỗi bản ghi
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String name = rs.getString("name");
-                    String mail = rs.getString("email");  // Lấy giá trị của cột mail
-                    float avgScore = rs.getFloat("avg_grade");  // Lấy giá trị của cột avg_score
-                    System.out.println("ID: " + id + ", Name: " + name + ", Mail: " + mail + ", Avg Score: " + avgScore);
-                }
-            }
+        try(Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println(conn.getCatalog());
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
