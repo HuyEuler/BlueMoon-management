361dcd8dc4331e5fafdaef591c0baab480608647
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-12 14:06:09 +0700

    -

---
+++ src/main/java/com/example/bluemoonmanagement/controllers/Home_management/editInformationController.java
@@ -0,0 +1,21 @@
+package com.example.bluemoonmanagement.controllers.Home_management;
+import com.example.bluemoonmanagement.api.FeeAPI;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.sql.Date;
+
+public class editInformationController {
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
