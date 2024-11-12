8de28915ebf658c0995518dc5bef20327615b779
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-13 01:12:36 +0700

    Create a file for global variables

---
+++ src/main/java/com/example/bluemoonmanagement/common/globalVariable.java
@@ -0,0 +1,8 @@
+package com.example.bluemoonmanagement.common;
+
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+
+public class globalVariable {
+    public static Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+}
