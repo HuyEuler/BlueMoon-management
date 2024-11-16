68d3643e54a2aad0f9f200f7f6a0d02224598ff3
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-16 08:30:26 +0700

    Khai báo USER

--- src/main/java/com/example/bluemoonmanagement/common/GlobalVariable.java
+++ src/main/java/com/example/bluemoonmanagement/common/GlobalVariable.java
@@ -1,11 +1,16 @@
 package com.example.bluemoonmanagement.common;
 
+import com.example.bluemoonmanagement.models.User;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
+
+import static com.example.bluemoonmanagement.api.UserAPI.getUser;
 
 public class GlobalVariable {
     public static Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
     public static double screenWidth = screenBounds.getWidth() * 0.75;
     public static double screenHeight = screenBounds.getHeight() * 0.75;
+
+    public static User USER = getUser(1);
 
 }
