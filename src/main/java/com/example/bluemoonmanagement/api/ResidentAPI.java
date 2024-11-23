7f0bc2f38ea28c39dc1751dcf9993aabbae1360d
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-23 16:31:10 +0700

    new

--- src/main/java/com/example/bluemoonmanagement/api/ResidentAPI.java
+++
@@ -1,5 +0,0 @@
-package com.example.bluemoonmanagement.api;
-
-public class ResidentAPI {
-
-}
---
+++ src/main/java/com/example/bluemoonmanagement/controllers/Resident_management/showOwner_controller.java
@@ -0,0 +1,13 @@
+package com.example.bluemoonmanagement.controllers.Resident_management;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class showOwner_controller {
+    @FXML
+    public Label numberRoom;
+
+    public void initialize() {
+
+    }
+}
