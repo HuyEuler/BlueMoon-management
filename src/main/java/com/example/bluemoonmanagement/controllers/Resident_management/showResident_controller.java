1f4c9a216a45269177d32448d09185392f0ef084
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-24 16:53:56 +0700

    new

--- src/main/java/com/example/bluemoonmanagement/controllers/Resident_management/showResident_controller.java
+++ src/main/java/com/example/bluemoonmanagement/controllers/Resident_management/showResident_controller.java
@@ -1,4 +1,23 @@
 package com.example.bluemoonmanagement.controllers.Resident_management;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class showResident_controller {
+    @FXML
+    private TextField Name;
+    @FXML
+    private TextField NumberPhone;
+    @FXML
+    private TextField Birthday;
+    @FXML
+    private TextField Sex;
+    @FXML
+    private TextField NumberApartment;
+    @FXML
+    private TextField Relationship;
+
+    public void initialize() {
+
+    }
 }
