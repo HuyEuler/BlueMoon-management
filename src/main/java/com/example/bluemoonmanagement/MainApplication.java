d25e4af54b58596cbb418202fff6c3ee2722bcb9
Nguyen Quang Dong <nguyendong.vn2004@gmail.com>
2024-11-25 18:29:07 +0700

    -

--- src/main/java/com/example/bluemoonmanagement/MainApplication.java
+++ src/main/java/com/example/bluemoonmanagement/MainApplication.java
@@ -13,29 +13,12 @@ import static com.example.bluemoonmanagement.common.GlobalVariable.*;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/GUILogin.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(),screenWidth, screenHeight);
-//        stage.setTitle("Apartment Management System");
-//
-//        stage.setScene(scene);
-//        stage.show();
-
-//        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/resident_management/show_owner.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load());
-//        stage = new Stage();
-//        stage.setScene(scene);
-//        stage.setTitle("Apartment Management System");
-//        stage.setResizable(false);
-//        stage.show();
-//
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/resident_management/show_activity.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage = new Stage();
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/GUILogin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),screenWidth, screenHeight);
         stage.setTitle("Apartment Management System");
-        stage.setResizable(false);
+
+        stage.setScene(scene);-
         stage.show();
-    }
 
     public static void main(String[] args) {
         launch();
