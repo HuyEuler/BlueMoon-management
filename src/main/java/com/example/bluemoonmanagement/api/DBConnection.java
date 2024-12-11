52fbf1841e9437b41bf805efd96ba6a19a53ca63
HuyEuler~ <duchuydtnn@gmail.com>
2024-12-11 23:08:06 +0700

    demo version

--- src/main/java/com/example/bluemoonmanagement/api/DBConnection.java
+++ src/main/java/com/example/bluemoonmanagement/api/DBConnection.java
@@ -11,7 +11,6 @@ private static final String URL = "jdbc:mysql://localhost:3306/bluemoon_project"
     private static final String USER = "root";
     private static final String PASSWORD = "";
     public static Connection getConnection() throws SQLException {
-//        return DriverManager.getConnection(URL, USER, PASSWORD);
         Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
 //        System.out.println("Connection established successfully");
         return connection;
@@ -25,9 +24,4 @@ public static void main(String[] args) {
             e.printStackTrace();
         }
     }
-
-//    public static void main(String[] args) {
-//        DBConnection connection1 = new DBConnection();
-//        connection1.connectToDatabase();
-//    }
 }
